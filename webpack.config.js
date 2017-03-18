181a3794e581b9230054978096ec1e182e75658f
Zhao,Chengdong <zhaochengdong@baidu.com>
2017-03-18 12:50:38 +0800

    第一交提交

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+var htmlWebpackPlugin = require('html-webpack-plugin');
+var path = require('path');
+const webpack = require('webpack');
+const autoprefixer = require('autoprefixer');
+
+module.exports = {
+    context: __dirname,
+    entry: {
+        main: './src/app.js',
+    },
+    output: {
+        path: './dist/js/',
+        filename: '[name]-bundle.js'
+    },
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            exclude: path.resolve(__dirname, 'node_modules'),
+            loader: 'babel-loader',
+            query: {
+                presets: ['latest']
+            }
+        }, {
+            test: /\.html$/,
+            loader: 'html-loader'
+        }, {
+            test: /\.css$/,
+            loader: 'style-loader!css-loader!postcss-loader',
+        }]
+    },
+    plugins: [
+        new htmlWebpackPlugin({
+            filename: 'index.html',
+            template: 'index.html',
+            title: 'webpack is good',
+            date: new Date(),
+            hehe: 'hehe',
+            minify: {
+                removeComments: true,
+            }
+        }),
+        new webpack.LoaderOptionsPlugin({
+            options: {
+                postcss: function() {
+                    return [autoprefixer];
+                }
+            }
+        })
+    ]
+}
